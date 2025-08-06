eb2269f0dee5df08eb7a2c585a547f415c0c99d1
Sarthak Bisht <bishtsarthak80@gmail.com>
2025-08-07 01:40:02 +0530

    movie app added

---
+++ Movie-app/netflix-clone/src/config.js
@@ -0,0 +1,26 @@
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCEy6YUeAvjOmRARvTRdfGeL8RJDzs8U7A",
+//     authDomain: "netflix-clone-d1fa8.firebaseapp.com",
+//     projectId: "netflix-clone-d1fa8",
+//     storageBucket: "netflix-clone-d1fa8.appspot.com",
+//     messagingSenderId: "1080602120235",
+//     appId: "1:1080602120235:web:29e7c05415df6c1cd65a6e"
+//   };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYdSTLQDize-3mquSg0Wk7MLJqYwvYuFs",
+  authDomain: "netflix-clone-65cb6.firebaseapp.com",
+  projectId: "netflix-clone-65cb6",
+  storageBucket: "netflix-clone-65cb6.firebasestorage.app",
+  messagingSenderId: "961173947346",
+  appId: "1:961173947346:web:b030f16c749d173cc13e0b",
+  measurementId: "G-61W09006Z9"
+};
+
+
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYTA2NWM4ZmNiMjBiMmJjYWY0NDJiMmRjYjE0NTRkOSIsIm5iZiI6MTc1NDQ3NTYxMi4wMjMsInN1YiI6IjY4OTMyYzVjMjdkODZjODRjZmZmZDM2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nbA9VzwM6mA0ko1SjXPCqHawjBrI-8VYMiS-FdprSJo";
+
+  export {firebaseConfig, TMDB_Access_Key};
